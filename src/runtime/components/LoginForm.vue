1c6f70c2c20fab49e8dd0b53b8b49e70bf8ba75c
Rodrigo Tejero <rodrigo.tejero@chimp.mx>
2024-01-22 18:51:24 -0600

    ✨ Added main.scss, ColorModeSwitch.vue, LoginForm.vue, and UserDropdown.vue
    - Added main.scss file with CSS variables and global styles.
    - Created ColorModeSwitch component for toggling color modes.
    - Created LoginForm component for user login form.
    - Created UserDropdown component for displaying user information and dropdown menu.

---
+++ src/runtime/components/LoginForm.vue
@@ -0,0 +1,61 @@
+<template>
+	<form>
+		<h3 class="mb-0">Welcome</h3>
+		<p class="text-muted">Ready to change the world?</p>
+
+		<div class="form-floating mb-3">
+			<input
+				type="email"
+				class="form-control form-control-login"
+				id="email"
+				autocomplete="username"
+				placeholder="Email"
+			>
+			<label for="email" class="form-label">Email</label>
+		</div>
+		<div class="form-floating mb-3">
+			<input
+				type="password"
+				class="form-control form-control-login"
+				id="password"
+				autocomplete="current-password"
+				placeholder="Password"
+			>
+			<label for="password" class="form-label">Password</label>
+		</div>
+		<button
+			type="submit"
+			class="btn btn-primary py-3 w-100 btn-login"
+		>Start the adventure
+		</button>
+	</form>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="sass" scoped>
+
+	.form-control-login
+		border-radius: 0
+		border-width: 0 0 1px
+		box-shadow: none
+		padding-left: 0
+		padding-right: 0
+
+	.btn-login
+		border-radius: 0
+
+	.form-floating > label
+		padding-left: 0
+		padding-right: 0
+		left: -0.15rem
+
+	input:-webkit-autofill,
+	input:-webkit-autofill:hover,
+	input:-webkit-autofill:focus,
+	input:-webkit-autofill:active
+		-webkit-box-shadow: 0 0 0 30px var(--bs-body-bg) inset !important
+
+</style>
---
+++ src/runtime/components/UserDropdown.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="site-user ms-auto d-flex align-items-center p-2 position-relative">
+		<p class="mb-0">Welcome <strong>Admin</strong></p>
+		<img
+			src="https://leadsapi.ag1.tech/users/1/avatar"
+			class="avatar ratio ratio-1x1 rounded-circle ms-2"
+			alt=""
+		>
+		<div class="user-menu position-absolute top-100 w-100 end-0">
+			<ul class="dropdown-menu">
+				<li>
+					<a
+						class="dropdown-item"
+						href="#"
+					>Logout</a>
+				</li>
+				<li class="p-2">
+					<color-mode-switch class="w-100" />
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script setup>
+	import ColorModeSwitch from './ColorModeSwitch.vue';
+</script>
+
+<style lang="sass" scoped>
+
+	.site-user
+		.avatar
+			width: 30px
+			border: 1px solid var(--bs-border-color)
+
+		&:hover
+			.user-menu
+				.dropdown-menu
+					border-top: 1px solid var(--bs-border-color)
+					border-top-left-radius: 0
+					border-top-right-radius: 0
+					display: block
+</style>
